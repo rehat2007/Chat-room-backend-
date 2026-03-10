5868c837bb77bfddc02c9311c857b6cc97f0137f
user. Rehat <additislam@gmail.com>
2026-03-10 22:14:48 +0600

    Edit profile route and controler setup done

---
+++ src/controller/editprofile.controller.js
@@ -0,0 +1,7 @@
+import { asyncHandler } from "../utils/async.Handler.js";
+
+const editProfile = asyncHandler( async ( req, res )=>{
+
+})
+
+export { editProfile }
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,11 +1,16 @@
 import { Router } from "express";
 import { registeruser, loginuser, logoutuser } from "../controller/user.controller.js"
+import { editProfile } from "../controller/editprofile.controller.js";
 
 const router = Router()
 
+// Auth routes :
 router.route("/register").post(registeruser)
 router.route("/login").post(loginuser)
 router.route("/logout").post(logoutuser)
+
+// Edit profile route :
+router.route("/profile").post(editProfile)
 
 
 
